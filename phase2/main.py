7e5290c91eaac3be722e06f67854e5b4bdd09f83
Ilia <iliashadravan84@gmail.com>
2025-06-30 12:01:28 +0330

    Update main.py

--- phase2/main.py
+++ phase2/main.py
@@ -1,15 +1,14 @@
-# main.py
-
 import os
 from input_reader import read_input
 from fcfs_scheduler import fcfs_scheduler
 
-
 def main():
     file_path = os.path.join("..", "phase1", "input.txt")
-
     processes = read_input(file_path)
-    fcfs_scheduler(processes)
+
+    total_resources = [0, 1, 0]
+
+    fcfs_scheduler(processes, total_resources)
 
 if __name__ == '__main__':
     main()
